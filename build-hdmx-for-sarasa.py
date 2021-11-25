8c2bb1328d2ceb6e73175f37f1ea65d941d42480
jonz94 <jody16888@gmail.com>
2021-11-25 18:24:11 +0800

    🚧 WIP: work in progress...

--- build-hdmx-for-sarasa.py
+++ build-hdmx-for-sarasa.py
@@ -18,44 +18,23 @@ filename = sys.argv[1]
 
 font = TTFont(filename, recalcBBoxes=False)
 
-head_ = font["head"]
-hmtx_ = font["hmtx"]
-name_ = font["name"]
+originFontHead = font["head"]
+originFontHmtx = font["hmtx"]
 
-head_.flags |= headFlagInstructionsMayAlterAdvanceWidth
+originFontHead.flags |= headFlagInstructionsMayAlterAdvanceWidth
 
-for entry in name_.names:
-    if entry.nameID in [1, 3, 4, 16, 18, 21]:
-        entry.string = (
-            "hdmx: ".encode(
-                "utf_16_be"
-                if entry.platformID == 3 and entry.platEncID in [1, 10]
-                else "ascii"
-            )
-            + entry.string
-        )
-    if entry.nameID in [6, 20]:
-        entry.string = (
-            "hdmx:".encode(
-                "utf_16_be"
-                if entry.platformID == 3 and entry.platEncID in [1, 10]
-                else "ascii"
-            )
-            + entry.string
-        )
-
-hdmx_ = newTable("hdmx")
-hdmx_.hdmx = {}
+hdmxTable = newTable("hdmx")
+hdmxTable.hdmx = {}
 
 # build hdmx table for odd and hinted ppems only.
 for ppem in range(math.floor(sarasaHintPpemMin / 2) * 2 + 1, sarasaHintPpemMax + 1, 2):
-    halfUpm = head_.unitsPerEm / 2
+    halfUpm = originFontHead.unitsPerEm / 2
     halfPpem = math.ceil(ppem / 2)
-    hdmx_.hdmx[ppem] = {
+    hdmxTable.hdmx[ppem] = {
         name: math.ceil(width / halfUpm) * halfPpem
-        for name, (width, _) in hmtx_.metrics.items()
+        for name, (width, _) in originFontHmtx.metrics.items()
     }
 
-font["hdmx"] = hdmx_
+font["hdmx"] = hdmxTable
 
 font.save("hdmx-" + filename)
